9b3c8ac710740d6afd66a4cf5a066144bb905aa4
surilindur <surilindur@users.noreply.github.com>
2023-07-13 18:23:37 +0300

    Change to run multiple configs at once

--- packages/query-executor/lib/QueryExecutionManager.ts
+++ packages/query-executor/lib/QueryExecutionManager.ts
@@ -1,14 +1,15 @@
 import { readFileSync, writeFileSync } from 'node:fs';
 import { join, basename } from 'node:path';
-import { QueryEngineFactory } from '@comunica/query-sparql';
+import { QueryEngineFactory, type QueryEngine } from '@comunica/query-sparql';
 import type { IBindingsHashFactory } from '@solidlab/chronomunica-bindings-hash';
 import type { IRequestCounterFactory } from '@solidlab/chronomunica-request-counter';
+import type { IQueryExecutionOutput } from './QueryExecution';
 import { QueryExecution } from './QueryExecution';
 
 export class QueryExecutionManager implements IQueryExecutionManager {
   private readonly queryFiles: string[];
   private readonly queryContext: Record<string, any> | undefined;
-  private readonly queryEngineConfig: string;
+  private readonly queryEngineConfigs: string[];
   private readonly resultSerializationPath: string;
   private readonly repeatExecution: number;
 
@@ -22,45 +23,64 @@ export class QueryExecutionManager implements IQueryExecutionManager {
     this.repeatExecution = args.repeatExecution;
     this.resultSerializationPath = args.resultSerializationPath;
     this.queryEngineFactory = new QueryEngineFactory();
-    this.queryEngineConfig = args.queryEngineConfig;
+    this.queryEngineConfigs = args.queryEngineConfigs;
     this.queryContext = args.queryContext;
     this.queryFiles = args.queryFiles;
   }
 
   public async execute(): Promise<void> {
-    for (const queryFile of this.queryFiles) {
-      const queryFileResults = [];
-      let errorOccurred = false;
-      for (let repeat = 0; repeat < this.repeatExecution; repeat++) {
-        const execution = new QueryExecution({
-          engine: await this.queryEngineFactory.create({ configPath: this.queryEngineConfig }),
-          bindingsHash: this.bindingsHashFactory.create(),
-          fetchCounter: this.requestCounterFactory.create(),
-          query: readFileSync(queryFile, { encoding: 'utf-8' }),
-          context: this.queryContext,
-        });
-        let executionError: string | undefined;
-        try {
-          console.log(`Execute: ${queryFile}`);
-          await execution.collect();
-        } catch (error: unknown) {
-          errorOccurred = true;
-          executionError = String(error);
-        }
-        const output = execution.metrics(executionError);
-        queryFileResults.push({
-          query: queryFile,
-          engine: this.queryEngineConfig,
-          ...output,
-        });
-        if (errorOccurred) {
-          break;
-        }
-      }
-      const outputPath = join(this.resultSerializationPath, `${basename(queryFile).split('.')[0]}.json`);
-      console.log(`Results: ${outputPath} (error: ${errorOccurred})`);
-      writeFileSync(outputPath, JSON.stringify(queryFileResults, undefined, 2), { encoding: 'utf-8' });
+    for (const configPath of this.queryEngineConfigs) {
+      const queryEngine = await this.queryEngineFactory.create({ configPath });
+      await this.executeForConfig(configPath, queryEngine);
     }
+  }
+
+  private async executeForConfig(configPath: string, queryEngine: QueryEngine): Promise<void> {
+    for (const queryFile of this.queryFiles) {
+      const queryString = readFileSync(queryFile, { encoding: 'utf-8' });
+      const results = await this.executeForConfigAndQuery(configPath, queryFile, queryString, queryEngine);
+      const outputPath = join(this.resultSerializationPath, `${basename(queryFile).split('.')[0]}.json`);
+      console.log(`Results: ${outputPath} (error: ${results.some(res => res.error !== undefined)})`);
+      writeFileSync(outputPath, JSON.stringify(results, undefined, 2), { encoding: 'utf-8' });
+    }
+  }
+
+  private async executeForConfigAndQuery(
+    configPath: string,
+    queryFile: string,
+    queryString: string,
+    queryEngine: QueryEngine,
+  ): Promise<IQueryExecutionOutput[]> {
+    const queryFileResults = [];
+    let errorOccurred = false;
+    for (let repeat = 0; repeat < this.repeatExecution; repeat++) {
+      await queryEngine.invalidateHttpCache();
+      const execution = new QueryExecution({
+        engine: queryEngine,
+        bindingsHash: this.bindingsHashFactory.create(),
+        fetchCounter: this.requestCounterFactory.create(),
+        query: queryString,
+        context: this.queryContext,
+      });
+      let executionError: string | undefined;
+      try {
+        console.log(`Execute: ${queryFile}`);
+        await execution.collect();
+      } catch (error: unknown) {
+        errorOccurred = true;
+        executionError = String(error);
+      }
+      const output = execution.metrics(executionError);
+      queryFileResults.push({
+        query: queryFile,
+        engine: configPath,
+        ...output,
+      });
+      if (errorOccurred) {
+        break;
+      }
+    }
+    return queryFileResults;
   }
 }
 
@@ -73,7 +93,7 @@ export interface IQueryExecutionManagerArgs {
   requestCounterFactory: IRequestCounterFactory;
   resultSerializationPath: string;
   repeatExecution: number;
-  queryEngineConfig: string;
+  queryEngineConfigs: string[];
   queryContext?: Record<string, any>;
   queryFiles: string[];
 }
