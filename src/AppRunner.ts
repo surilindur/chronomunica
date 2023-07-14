82de384065226e35bf18f4ef4bfd7f9f692aeaa9
surilindur <surilindur@users.noreply.github.com>
2023-07-14 11:07:10 +0300

    Remove Components.js

--- src/AppRunner.ts
+++ src/AppRunner.ts
@@ -1,40 +1,35 @@
 import { resolve } from 'node:path';
 import { env } from 'node:process';
 import { parseArgs } from 'node:util';
-import { ComponentsManager } from 'componentsjs';
-import type { IMeasurementRunner } from './MeasurementRunner';
+import { MeasurementRunner, type IMeasurementRunner } from './MeasurementRunner';
+import { BindingsHash, MeasurementSerializer, RequestCounter } from '.';
 
-const defaultRunner = 'urn:chronomunica:runner#default';
 const defaultConfig = resolve(__dirname, '..', 'config', 'default.json');
-const defaultMainModulePath = resolve(__dirname);
 
 export async function runApp(): Promise<void> {
-  let { config, engine, query, context, runner, modules } = parseArgs({
+  let { engine, query, context } = parseArgs({
     options: {
-      config: { type: 'string', default: defaultConfig },
       engine: { type: 'string' },
       query: { type: 'string' },
       context: { type: 'string' },
-      runner: { type: 'string', short: 'e', default: defaultRunner },
-      modules: { type: 'string', short: 'm', default: defaultMainModulePath },
     },
   }).values;
 
-  config = env.CHRONOMUNICA_CONFIG ?? config;
   engine = env.CHRONOMUNICA_ENGINE ?? engine;
-  runner = env.CHRONOMUNICA_RUNNER ?? runner;
-  modules = env.CHRONOMUNICA_MODULE_PATH ?? modules;
   query = env.CHRONOMUNICA_QUERY ?? query;
   context = env.CHRONOMUNICA_CONTEXT ?? context;
 
-  const manager = await ComponentsManager.build({
-    mainModulePath: modules!,
-    typeChecking: false,
+  if (!engine || !query) {
+    throw new Error('Missing engine config or query file path');
+  }
+
+  const measurementRunner: IMeasurementRunner = new MeasurementRunner({
+    bindingsHash: new BindingsHash({ algorithm: 'sha256', encoding: 'hex' }),
+    measurementSerializer: new MeasurementSerializer({ outputPath: '/results' }),
+    requestCounter: new RequestCounter(),
   });
 
-  await manager.configRegistry.register(config!);
-  const queryRunner = await manager.instantiate<IMeasurementRunner>(runner!);
-  await queryRunner.run(engine!, query!, context);
+  await measurementRunner.run(engine, query, context);
 }
 
 export function runAppStatic(): void {
