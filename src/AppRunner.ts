5d6166a510423158c3040908e3dabd5076c45eaf
surilindur <surilindur@users.noreply.github.com>
2023-07-14 11:49:37 +0300

    Fix parameter name

--- src/AppRunner.ts
+++ src/AppRunner.ts
@@ -4,12 +4,12 @@ import { MeasurementRunner, type IMeasurementRunner } from './MeasurementRunner'
 import { BindingsHash, MeasurementSerializer, RequestCounter } from '.';
 
 export async function runApp(): Promise<void> {
-  let { engine, query, context, results, hash, digest } = parseArgs({
+  let { engine, query, context, output, hash, digest } = parseArgs({
     options: {
       engine: { type: 'string' },
       query: { type: 'string' },
       context: { type: 'string' },
-      results: { type: 'string' },
+      output: { type: 'string' },
       hash: { type: 'string', default: 'sha256' },
       digest: { type: 'string', default: 'hex' },
     },
@@ -18,17 +18,17 @@ export async function runApp(): Promise<void> {
   engine = env.CHRONOMUNICA_ENGINE ?? engine;
   query = env.CHRONOMUNICA_QUERY ?? query;
   context = env.CHRONOMUNICA_CONTEXT ?? context;
-  results = env.CHRONOMUNICA_RESULTS ?? results;
+  output = env.CHRONOMUNICA_RESULTS ?? output;
   hash = env.CHRONOMUNICA_HASH ?? hash;
   digest = env.CHRONOMUNICA_DIGEST ?? digest;
 
-  if (!engine || !query || !results) {
+  if (!engine || !query || !output) {
     throw new Error('Missing engine config, query file path or results path');
   }
 
   const measurementRunner: IMeasurementRunner = new MeasurementRunner({
     bindingsHash: new BindingsHash({ algorithm: hash, encoding: digest }),
-    measurementSerializer: new MeasurementSerializer({ path: results }),
+    measurementSerializer: new MeasurementSerializer({ path: output }),
     requestCounter: new RequestCounter(),
   });
 
